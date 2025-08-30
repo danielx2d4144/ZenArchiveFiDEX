8451579977a609f1336c2d7e92cd7f152c67196f
rdx2 <Josiah.M2203191@st.futminna.edu.ng>
2025-08-30 20:29:01 +0100

    Add ThirdWeb client ID

--- utils/thirdweb.ts
+++ utils/thirdweb.ts
@@ -1,9 +1,9 @@
 import { createThirdwebClient } from "thirdweb";
 import { defineChain } from "thirdweb/chains";
 
-// Create the thirdweb client
-export const client = createThirdwebClient({ 
-  clientId: process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || "" 
+// Create the thirdweb client with your client ID
+export const client = createThirdwebClient({
+  clientId: process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || "eb6392fc427e0b637a5d2c61513c2da8"
 });
 
 const chain = defineChain({
